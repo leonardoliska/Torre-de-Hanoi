aadd5d57126278115ddb9d2718163ee87bdd1ad2
leonardoliska <leo.liska@hotmail.com>
2021-11-29 22:20:09 -0300

    Tela de dificuldade adicionada pelo DOM

--- js/index.js
+++ js/index.js
@@ -35,12 +35,42 @@ function createPieces(){
         piece.style.backgroundColor = pieceColors[i-1]
 
         //Adicionar Width
-        piece.style.width = `${(80 / dificulty) * i}%`
+        piece.style.width = `${(40 / dificulty) * i + 20}%`
 
         //Posicionar elementos em seus respectivos Parents
         const tower = document.getElementById("tower-1")
         tower.appendChild(piece)
-
-        
     }
+}
+
+function createDifficultyScreen() {
+    
+    // Criar elementos
+    const difficultyContainer = document.createElement('div')
+    const difficultyTitle = document.createElement('h2')
+    const easyButton = document.createElement('button')
+    const mediumButton = document.createElement('button')
+    const hardButton = document.createElement('button')
+
+    // Adicionar classes e ID's aos elementos
+    difficultyContainer.id = 'difficulty'
+    difficultyTitle.classList.add('difficulty-title')
+    easyButton.classList.add('difficulty-button')
+    mediumButton.classList.add('difficulty-button')
+    hardButton.classList.add('difficulty-button')
+
+    // Adicionar texto aos Elementos
+    difficultyTitle.innerText = 'Selecione a Dificuldade'
+    easyButton.innerText = 'Fácil'
+    mediumButton.innerText = 'Médio'
+    hardButton.innerText = 'Difícil'
+
+    // Adicionar elementos em seus respectivos Parents
+    difficultyContainer.appendChild(difficultyTitle)
+    difficultyContainer.appendChild(easyButton)
+    difficultyContainer.appendChild(mediumButton)
+    difficultyContainer.appendChild(hardButton)
+
+    const gameContainer = document.getElementById('game-container')
+    gameContainer.appendChild(difficultyContainer)
 }
